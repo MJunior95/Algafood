1801979539655fcfbe0a4755af9b42b6a3eabc96
Michel Junior <jrcassunde95@gmail.com>
2021-03-22 21:32:09 -0300

    aprendendo metodos @INIT

--- src/main/java/com/algaworks/algafood/di/notificacao/NotificadorEmail.java
+++ src/main/java/com/algaworks/algafood/di/notificacao/NotificadorEmail.java
@@ -1,5 +1,8 @@
 package com.algaworks.algafood.di.notificacao;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 import org.springframework.stereotype.Component;
 
 import com.algaworks.algafood.di.modelo.Cliente;
@@ -7,6 +10,21 @@ import com.algaworks.algafood.di.modelo.Cliente;
 @TipoDoNotificador(NivelUrgencia.SEM_URGENCIA)
 @Component
 public class NotificadorEmail implements Notificador {
+
+	public NotificadorEmail() {
+		System.out.println("Notificador Email REAL");
+
+	}
+	
+	@PostConstruct
+	public void init() {
+		System.out.println("INIT");
+	}
+	
+	@PreDestroy
+	public void destroy() {
+		System.out.println("DESTROY");
+	}
 
 	@Override
 	public void notificar(Cliente cliente, String mensagem) {
