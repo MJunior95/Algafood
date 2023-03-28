3bc45d480c82d78a361c961b50328cb10b774369
Michel Junior <jrcassunde95@gmail.com>
2023-03-28 18:54:15 -0300

    avançando no spring data jpa

--- src/main/java/com/algafood/domain/repository/CozinhaRepository.java
+++ src/main/java/com/algafood/domain/repository/CozinhaRepository.java
@@ -1,5 +1,8 @@
 package com.algafood.domain.repository;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,6 +10,12 @@ import com.algafood.domain.model.Cozinha;
 
 @Repository
 public interface CozinhaRepository extends JpaRepository<Cozinha, Long>{
+	
+	List<Cozinha> findAllByNomeContaining(String nome);
+	
+	Optional<Cozinha> findByNome(String nome);
 
+	boolean existsByNome(String nome);
+	
 	
 }
