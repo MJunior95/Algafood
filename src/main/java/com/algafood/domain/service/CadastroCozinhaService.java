6b19477b4ae7e413ebf6c8337fbf702a0fd8fe30
Michel Junior <jrcassunde95@gmail.com>
2023-04-05 18:27:20 -0300

    tratando exception com ResponseStatusException

--- src/main/java/com/algafood/domain/service/CadastroCozinhaService.java
+++ src/main/java/com/algafood/domain/service/CadastroCozinhaService.java
@@ -3,9 +3,10 @@ package com.algafood.domain.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
-import com.algafood.domain.exception.EntidadeEmUsoException;
 import com.algafood.domain.exception.EntidadeNaoEncotradaException;
 import com.algafood.domain.model.Cozinha;
 import com.algafood.domain.repository.CozinhaRepository;
@@ -25,8 +26,10 @@ public class CadastroCozinhaService {
 			repository.deleteById(cozinhaId);
 
 		} catch (DataIntegrityViolationException e) {
-			throw new EntidadeEmUsoException(
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND,
 					String.format("Cozinha de código %d não pode ser removida, pois está em uso", cozinhaId));
+		//	throw new EntidadeEmUsoException(
+			//		String.format("Cozinha de código %d não pode ser removida, pois está em uso", cozinhaId));
 		} catch (EmptyResultDataAccessException e) {
 			throw new EntidadeNaoEncotradaException(
 					String.format("Não existe um cadastro de cozinha com código: %d", cozinhaId));
