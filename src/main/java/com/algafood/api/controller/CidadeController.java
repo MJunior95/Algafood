5f3cb31e6271cc671d1b160b2e7beaceb5fab811
Michel Junior <jrcassunde95@gmail.com>
2023-04-11 18:39:39 -0300

    criando o exception handler para tratar as exceptions do projeto

--- src/main/java/com/algafood/api/controller/CidadeController.java
+++ src/main/java/com/algafood/api/controller/CidadeController.java
@@ -74,5 +74,7 @@ public class CidadeController {
 		service.excluir(id);
 
 	}
+	
+
 
 }
--- src/main/java/com/algafood/domain/repository/CozinhaRepository.java
+++ src/main/java/com/algafood/domain/repository/CozinhaRepository.java
@@ -4,11 +4,9 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import com.algafood.domain.model.Cozinha;
 
-@Repository
 public interface CozinhaRepository extends JpaRepository<Cozinha, Long>{
 	
 	List<Cozinha> findAllByNomeContaining(String nome);
