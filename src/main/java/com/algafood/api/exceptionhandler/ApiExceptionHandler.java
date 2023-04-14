7d5834a7a9034c46fcccf9f48f6b675c06e4cfcb
Michel Junior <jrcassunde95@gmail.com>
2023-04-13 21:18:43 -0300

    correcoes exceptions

--- src/main/java/com/algafood/api/exceptionhandler/ApiExceptionHandler.java
+++ src/main/java/com/algafood/api/exceptionhandler/ApiExceptionHandler.java
@@ -1,11 +1,8 @@
 package com.algafood.api.exceptionhandler;
 
-import java.time.LocalDateTime;
-
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.HttpMediaTypeNotSupportedException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
@@ -16,41 +13,64 @@ import com.algafood.domain.exception.EntidadeNaoEncontradaException;
 import com.algafood.domain.exception.NegocioException;
 
 @ControllerAdvice
-public class ApiExceptionHandler extends ResponseEntityExceptionHandler{
-	
+public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
+
 	@ExceptionHandler(EntidadeNaoEncontradaException.class)
-	public ResponseEntity<?> tratarEntidadeNaoEncontradoException(
-			EntidadeNaoEncontradaException e, WebRequest request){
-		return handleExceptionInternal(
-				e, e.getMessage(), new HttpHeaders(), HttpStatus.NOT_FOUND,request);
-				
+	public ResponseEntity<?> handleEntidadeNaoEncontradoException(EntidadeNaoEncontradaException e,
+			WebRequest request) {
+
+		HttpStatus status = HttpStatus.NOT_FOUND;
+
+		ProblemType problemType = ProblemType.ENTIDADE_NAO_ENCONTRADA;
+		String detail = e.getMessage();
+
+		Problem problem = createProblemBuilder(status, problemType, detail).build();
+
+		return handleExceptionInternal(e, problem, new HttpHeaders(), status, request);
+
 	}
-	
+
 	@ExceptionHandler(EntidadeEmUsoException.class)
-	public ResponseEntity<?> tratarEntidadeEmUsoException(
-			EntidadeEmUsoException e, WebRequest request){
-		return handleExceptionInternal(
-				e, e.getMessage(), new HttpHeaders(), HttpStatus.CONFLICT,request);
-		
+	public ResponseEntity<?> handleEntidadeEmUsoException(EntidadeEmUsoException e, WebRequest request) {
+
+		HttpStatus status = HttpStatus.CONFLICT;
+
+		ProblemType problemType = ProblemType.ENTIDADE_EM_USO;
+		String detail = e.getMessage();
+
+		Problem problem = createProblemBuilder(status, problemType, detail).build();
+		return handleExceptionInternal(e, problem, new HttpHeaders(), status, request);
+
 	}
-	
+
 	@ExceptionHandler(NegocioException.class)
-	public ResponseEntity<?> tratarNegocioException(
-			NegocioException e, WebRequest request){
-		return handleExceptionInternal(
-				e, e.getMessage(), new HttpHeaders(), HttpStatus.BAD_REQUEST,request);
+	public ResponseEntity<?> handleNegocioException(NegocioException ex, WebRequest request) {
+
+		HttpStatus status = HttpStatus.BAD_REQUEST;
+		ProblemType problemType = ProblemType.ERRO_NEGOCIO;
+		String detail = ex.getMessage();
+
+		Problem problem = createProblemBuilder(status, problemType, detail).build();
+
+		return handleExceptionInternal(ex, problem, new HttpHeaders(), status, request);
 	}
-	
+
 	@Override
 	protected ResponseEntity<Object> handleExceptionInternal(Exception ex, Object body, HttpHeaders headers,
 			HttpStatus status, WebRequest request) {
-		
-		body = Problema.builder()
-				.dataHora(LocalDateTime.now())
-				.mensagem(body == null ? status.getReasonPhrase() : body.toString())
-				.build();
-		
+
+		if (body == null) {
+			body = Problem.builder().title(status.getReasonPhrase()).status(status.value()).build();
+		} else if (body instanceof String) {
+			body = Problem.builder().title((String) body).status(status.value()).build();
+		}
+
 		return super.handleExceptionInternal(ex, body, headers, status, request);
 	}
-	
+
+	private Problem.ProblemBuilder createProblemBuilder(HttpStatus status, ProblemType problemType, String detail) {
+
+		return Problem.builder().status(status.value()).type(problemType.getUri()).title(problemType.getTitle())
+				.detail(detail);
+	}
 }
