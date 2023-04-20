237be09e04c8a0b50e9b0bfb1fb6071f8b3d5b85
Michel Junior <jrcassunde95@gmail.com>
2023-04-20 16:23:25 -0300

    acrescentando teste de consultar por id

--- src/test/java/com/algafood/algafoodapi/CadastroCozinhaIT.java
+++ src/test/java/com/algafood/algafoodapi/CadastroCozinhaIT.java
@@ -20,6 +20,8 @@ import com.algafood.domain.service.CadastroCozinhaService;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 
+import static org.hamcrest.CoreMatchers.equalTo;
+
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @RunWith(SpringRunner.class)
 @TestPropertySource("/application-test.properties")
@@ -78,10 +80,33 @@ public class CadastroCozinhaIT {
 				.statusCode(HttpStatus.CREATED.value());
 	}
 
+	@Test
+	public void deveRetornarRespostaEStatusCorretosQuandoConsultarCozinhaExistente(){
+		RestAssured.given()
+				.pathParam("cozinhaId", 2)
+				.accept(ContentType.JSON)
+				.when()
+				.get("/{cozinhaId}")//Se refere ao metodo GET do HTTP
+				.then()
+				.statusCode(HttpStatus.OK.value())
+				.body("nome", equalTo("Tailandesa"));
+
+	}
+	@Test
+	public void deveRetornarStatus404QuandoConsultarCozinhaInexistente(){
+		RestAssured.given()
+				.pathParam("cozinhaId", 200)
+				.accept(ContentType.JSON)
+				.when()
+				.get("/{cozinhaId}")//Se refere ao metodo GET do HTTP
+				.then()
+				.statusCode(HttpStatus.NOT_FOUND.value());
+
+	}
+
 	private void prepararDados(){
 		Cozinha cozinha1 =  new Cozinha();
 		cozinha1.setNome("Indiana");
-
 		cozinhaRepository.save(cozinha1);
 
 		Cozinha cozinha2 =  new Cozinha();
