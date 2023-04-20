bf7c812b83f5fac7235d6af384a4072c18e4506e
Michel Junior <jrcassunde95@gmail.com>
2023-04-20 11:39:24 -0300

    implementando Rest Assured para realizar testes de api

--- src/test/java/com/algafood/algafoodapi/CadastroCozinhaIntegrationTests.java
+++ src/test/java/com/algafood/algafoodapi/CadastroCozinhaIntegrationTests.java
@@ -4,10 +4,14 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import javax.validation.ConstraintViolationException;
 
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import com.algafood.domain.exception.EntidadeEmUsoException;
@@ -15,43 +19,27 @@ import com.algafood.domain.exception.EntidadeNaoEncontradaException;
 import com.algafood.domain.model.Cozinha;
 import com.algafood.domain.service.CadastroCozinhaService;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @RunWith(SpringRunner.class)
 public class CadastroCozinhaIntegrationTests {
+
+	@LocalServerPort
+	private int Port;
 	
 	@Autowired
 	private CadastroCozinhaService cadastroCozinha;
 
 	@Test
-	public void deveCadastroCozinhaComSucesso() {
-		Cozinha novaCozinha = new Cozinha();
-		novaCozinha.setNome("Chinesa");
-		
-		novaCozinha = cadastroCozinha.salvar(novaCozinha);
-		
-		assertThat(novaCozinha).isNotNull();
-		assertThat(novaCozinha.getId()).isNotNull();
-	}
-	
-	@Test(expected = ConstraintViolationException.class)
-	public void deveFalharAoCadastrarCozinhaSemNome() {
-		Cozinha novaCozinha = new Cozinha();
-		novaCozinha.setNome(null);
-		
-		novaCozinha = cadastroCozinha.salvar(novaCozinha);
-		
-	}
-	
-	@Test(expected = EntidadeEmUsoException.class)
-	public void deveFalharAoExcluirCozinhaEmUso() {
-		Long idCozinhaEmUso = 1l;
-		cadastroCozinha.excluir(idCozinhaEmUso);
-	}
-	
-	@Test(expected = EntidadeNaoEncontradaException.class)
-	public void deveFalharAoExcluirCozinhaInexistente() {
-		Long idCozinhaInexistente = 1000l;
-		cadastroCozinha.excluir(idCozinhaInexistente);
-	}
+	public void deveRetornarStatus200QuandoConsultarCozinhas(){
+		RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
 
+		RestAssured.given()
+					.basePath("/cozinhas")
+					.port(8080)
+					.accept(ContentType.JSON)
+				.when()
+					.get()
+				.then()
+					.statusCode(HttpStatus.OK.value());
+	}
 }
